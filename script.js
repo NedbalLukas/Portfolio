bdd08e1af3761f0ae40d1d12d8cba54627374be7
lukas <lukned2005@gmail.com>
2024-11-08 20:19:53 +0100

    Update3

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-}
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-
-    
-    const email = event.target.querySelector("input[type='email']").value;
-
-    
-    alert(`Form submitted! Email: ${email}`);
 });
